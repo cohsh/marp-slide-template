4aff48bcfa87fd33808584b2e6c9dde5222cf4fa
cohsh <82075327+cohsh@users.noreply.github.com>
2024-06-03 18:16:19 +0900

    :zap: updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/marp-slide-template/' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/marp-slide-template' : '',
+    images: {
+        unoptimized: true,
+    },
+};
 
 export default nextConfig;
